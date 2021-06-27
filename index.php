d160696914bd865f65060a67d7ff301715a47dd6
Abbhishek N N <7abhisheknn@gmail.com>
2021-06-27 11:00:08 +0530

    done

--- index.php
+++ index.php
@@ -7,16 +7,32 @@ if(isset($_GET['c_p_id'])){
     $_SESSION['c_p_id']=$_GET['c_p_id'];
     header('Location: customer_login.php');
 }
+$place_k=array('p_id','a_company_name','a_email','p_price','a_country','a_state','a_district','a_pincode');
+$Go_S=array(0=>'',1=>'',2=>'',3=>'',4=>'',5=>'',6=>'',7=>'');
+
+/// if go clicked
+if(isset($_POST['Go'])){
+    for ($i=0; $i < count($place_k); $i++) { 
+        $Go_S[$i]=mysqli_real_escape_string($conn, $_POST[$place_k[$i]]);
+    }
+}
 
 /// getting places details to display
-$sql='SELECT p.p_id, p.a_id, p.p_price, p.p_from, p.p_till, p.v_no, a.a_email, a.a_name, a.a_company_name,a.a_email, a.a_country, a.a_state, a.a_district, a.a_address, a.a_pincode FROM place as p, admin as a WHERE p.a_id=a.a_id AND p.v_no IS NULL';
+// $sql="SELECT p.p_id, p.a_id, p.p_price, p.p_from, p.p_till, p.v_no, a.a_email, a.a_name, a.a_company_name,a.a_email, a.a_country, a.a_state, a.a_district, a.a_address, a.a_pincode FROM place as p, admin as a WHERE p.a_id=a.a_id AND p.v_no IS NULL AND p.p_id LIKE '%$Go_S[0]%' AND a.a_company_name LIKE '%Go_S[1]%' ";
+
+$sql="SELECT p.p_id, p.a_id, p.p_price, p.p_from, p.p_till, p.v_no, a.a_email, a.a_name, a.a_company_name,a.a_email, a.a_country, a.a_state, a.a_district, a.a_address, a.a_pincode FROM place as p, admin as a WHERE p.a_id=a.a_id AND p.v_no IS NULL AND p.p_id LIKE '%$Go_S[0]%' AND a.a_company_name LIKE '%$Go_S[1]%' AND a.a_email LIKE '%$Go_S[2]%' AND p.p_price LIKE '%$Go_S[3]%' AND a.a_country LIKE '%$Go_S[4]%' AND a.a_state LIKE '%$Go_S[5]%' AND a.a_district LIKE '%$Go_S[6]%' AND a.a_pincode LIKE '%$Go_S[7]%'";
 $result=mysqli_query($conn,$sql);
+// if (mysqli_query($conn,$sql)){
+//     echo "yay";
+// } else {
+//     echo 'query error: '. mysqli_error($conn);
+// }
 $places=mysqli_fetch_all($result,MYSQLI_ASSOC);
 mysqli_free_result($result);
 mysqli_close($conn);
 
 /// array to store data heading for in each place
-$place_k=array('p_id','a_company_name','a_email','p_price','a_country','a_state','a_district','a_pincode')
+
 ?>
 
 <?php include('template/header.php'); ?>
@@ -44,6 +60,25 @@ $place_k=array('p_id','a_company_name','a_email','p_price','a_country','a_state'
     </tr>
 </thead>
 <tbody>
+    <tr>
+    <form action="index.php" method="post">
+    <?php foreach($place_k as $k){
+        $selects = array_column($places,$k);
+        $selects_u = array_unique($selects);
+        ?>
+        <td>
+            <select name="<?php echo $k; ?>" >
+                <option value="">Select...</option>
+            <?php foreach($selects_u as $s) { ?>
+                <option value="<?php echo $s; ?>"><?php echo $s; ?></option>
+            <?php } ?>
+            </select>
+        </td>
+    <?php } ?>
+    <td><input type="submit" value="Go" name="Go"></td>
+    </form>
+    <td><a href="index.php">reset</a></td>
+    </tr>
 <?php foreach ($places as $place) {?>
     <tr>
         <?php
@@ -62,4 +97,5 @@ $place_k=array('p_id','a_company_name','a_email','p_price','a_country','a_state'
 <p>Parking Management System  -  A better way to manage and park our vehicles.</p>
 </div>
 </section>
+
 <?php include('template/footer.php'); ?>
